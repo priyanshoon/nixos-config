105a453776fa7851e81e3ff72cfeea5c846c6d85
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-06-23 18:22:05 +0530

    changed noctalia-shell

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         };
 
         noctalia = {
-            url = "github:noctalia-dev/noctalia-shell";
+            url = "github:noctalia-dev/noctalia/legacy-v4";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
