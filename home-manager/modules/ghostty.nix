9be9686356cba373504ef69d82359ffd6e31e1f1
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-07-24 07:24:38 +0530

    changed background opacity

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -7,7 +7,7 @@
             font-family = lib.mkForce "Mononoki Nerd Font";
             cursor-style = "block";
             font-size = 16;
-            background-opacity = 0.9;
+            # background-opacity = 0.9;
         };
     };
 }
