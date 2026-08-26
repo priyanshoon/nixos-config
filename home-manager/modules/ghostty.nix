279cf380c4373bad6e04f4eba650a83aee093c53
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-08-26 12:32:49 +0530

    changed theme to Catppuccin

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -3,14 +3,14 @@
         enable = true;
         settings = {
             # background = "#000000";
-            theme = "Vague";
+            theme = "Catppuccin Mocha";
             # background-image = "/home/priyanshoon/Pictures/Wallpapers/wallhaven-45gz65_1920x1080.png";
             font-family = lib.mkForce "Mononoki Nerd Font";
             cursor-style = "block";
             font-size = 15;
             # background-image-opacity = 0.8;
-            background-opacity = 0.9;
-            background-blur = true;
+            # background-opacity = 0.9;
+            # background-blur = true;
         };
     };
 }
--- home-manager/modules/nixvim/plugins/color.nix
+++ home-manager/modules/nixvim/plugins/color.nix
@@ -1,21 +1,23 @@
 {
     programs.nixvim = {
-        # colorschemes.kanagawa = {
-        #     enable = true;
-        #     settings = {
-        #         transparent = true;
-        #         dimInactive = true;
-        #         # theme = "dragon";
-        #         # background = {
-        #         #     dark = "dragon";
-        #         # };
-        #     };
-        # };
-       colorschemes.vague = {
-           enable = true;
-           settings = {
-               transparent = true;
-           };
-       };
+        colorschemes.catppuccin = {
+            enable = true;
+            callSetup = {
+                flavour = "mocha";
+                background = {
+                    dark = "mocha";
+                };
+                transparent_background = true;
+                dim_inactive = {
+                    enabled = true;
+                };
+            };
+        };
+       # colorschemes.vague = {
+       #     enable = true;
+       #     settings = {
+       #         transparent = true;
+       #     };
+       # };
     };
 }
