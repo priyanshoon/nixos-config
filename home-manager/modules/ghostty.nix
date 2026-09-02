d1320a884d84bf812988fc6b6456326cdc86d4ee
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-09-02 10:40:58 +0530

    changed font of ghostty

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -4,8 +4,8 @@
     settings = {
       theme = "Everforest Dark Hard";
       #ifont-family = lib.mkForce "Mononoki Nerd Font";
-      # font-family = lib.mkForce "FiraCode Nerd Font";
-      font-family = lib.mkForce "GeistMono Nerd Font";
+      font-family = lib.mkForce "FiraCode Nerd Font";
+      # font-family = lib.mkForce "GeistMono Nerd Font";
       cursor-style = "block";
       font-size = 15;
     };
